bc1ecf4f5896e8ec437f07df83f2eabe342412ee
Mathijs Marks <mathijsmarks@msn.com>
2021-06-22 22:26:47 +0200

    Created ProfileFragment class and layout file. Task 1 complete!

--- app/src/main/java/com/example/madlevel7task1/ProfileFragment.kt
+++ app/src/main/java/com/example/madlevel7task1/ProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.example.madlevel7task1
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel7task1.databinding.FragmentProfileBinding
 
@@ -19,11 +21,12 @@ class ProfileFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val viewModel: ProfileViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -31,10 +34,27 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observeProfile()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observeProfile() {
+        viewModel.getProfile()
+
+        viewModel.profile.observe(viewLifecycleOwner, {
+            val profile = it
+
+            binding.tvName.text =
+                getString(R.string.profile_name, profile.firstName, profile.lastName)
+            binding.tvDescription.text = profile.description
+            if (profile.imageUri!!.isNotEmpty()) {
+                binding.ivProfileImage.setImageURI(Uri.parse(profile.imageUri))
+            }
+        })
     }
 }
