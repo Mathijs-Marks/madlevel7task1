68d2d1cd888059fedd745abe8c741fa0ed072ddc
Mathijs Marks <mathijsmarks@msn.com>
2021-06-22 22:20:23 +0200

    Created CreateProfileFragment class and layout file.

--- app/src/main/java/com/example/madlevel7task1/CreateProfileFragment.kt
+++ app/src/main/java/com/example/madlevel7task1/CreateProfileFragment.kt
@@ -1,10 +1,15 @@
 package com.example.madlevel7task1
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.madlevel7task1.databinding.FragmentCreateProfileBinding
 
@@ -13,11 +18,15 @@ import com.example.madlevel7task1.databinding.FragmentCreateProfileBinding
  */
 class CreateProfileFragment : Fragment() {
 
+    private var profileImageUri: Uri? = null
+
     private var _binding: FragmentCreateProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel: ProfileViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,10 +40,71 @@ class CreateProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGallery.setOnClickListener { onGalleryClick() }
+        binding.btnConfirm.setOnClickListener { onConfirmClick() }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    profileImageUri = data?.data
+                    binding.ivProfileImage.setImageURI(profileImageUri)
+                }
+            }
+        }
+    }
+
+    private fun onGalleryClick() {
+        // Create an Intent with action as ACTION_PICK
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+
+        // Sets the type as image/*. This ensures only components of type image are selected.
+        galleryIntent.type = "image/*"
+
+        // Start the activity using the gallery intent.
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    companion object {
+        const val GALLERY_REQUEST_CODE = 100
+    }
+
+    // This is a helper method that passes some default values in the case that an EditText is left empty.
+    private fun CharSequence?.ifNullOrEmpty(default: String) =
+        if (this.isNullOrEmpty()) default else this.toString()
+
+    // This is a helper method that passes a default value if the profileImageUri is null.
+    private fun Uri?.ifNullOrEmpty() =
+        this?.toString() ?: ""
+
+    private fun onConfirmClick() {
+        viewModel.createProfile(
+            binding.etFirstName.text.ifNullOrEmpty(""),
+            binding.etLastName.text.ifNullOrEmpty(""),
+            binding.etProfileDescription.text.ifNullOrEmpty(""),
+            profileImageUri.ifNullOrEmpty()
+        )
+
+        observeProfileCreation()
+
+        findNavController().navigate(R.id.profileFragment)
+    }
+
+    private fun observeProfileCreation() {
+        viewModel.createSuccess.observe(viewLifecycleOwner, {
+            Toast.makeText(activity, R.string.successfully_created_profile, Toast.LENGTH_LONG).show()
+            findNavController().popBackStack()
+        })
+
+        viewModel.errorText.observe(viewLifecycleOwner, {
+            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+        })
     }
 }
